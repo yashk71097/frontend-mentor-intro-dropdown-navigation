64ef189e5e3a5942168546c873bd898b10837dbf
Yash Pathik <yashk71097@gmail.com>
2023-04-02 13:04:31 +0530

    Complete Intro with dropdown navigation webpage

---
+++ README.md
@@ -0,0 +1,69 @@
+# Frontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+    -   [What I learned](#what-i-learned)
+    -   [Useful resources](#useful-resources)
+-   [Author](#author)
+
+## Overview
+
+-   This is the static website of Intro section with dropdown navigation.
+
+### The challenge
+
+Users should be able to:
+
+-   View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+-   View the optimal layout for the content depending on their device's screen size
+-   See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./images/screenshot-1.png)
+![](./images/screenshot-2.png)
+
+### Links
+
+-   Solution URL: [Add solution URL here](https://your-solution-url.com)
+-   Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Vanilla Javascript
+-   Flexbox
+-   CSS Grid
+-   Utility Classes
+-   Mobile-first workflow
+
+### What I learned
+
+I got to know some javascript methods like contain and setAttribute.
+Here is te example of some...
+
+```js
+featuresDropdown.classList.contains("hidden");
+
+arrow[1].setAttribute("src", "../images/icon-arrow-down.svg");
+```
+
+### Useful resources
+
+-   [Example resource 1](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - This website helped me with javascript methods and functions.
+
+## Author
+
+-   Website - [Yash Pathik](https://www.your-site.com)
+-   Frontend Mentor - [@yashk71097](https://www.frontendmentor.io/profile/yashk71097)
---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+const menuButton = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".primary-navigation");
+
+const features = document.querySelector(".features");
+console.log(features);
+
+const featuresDropdown = document.querySelector(".features-dropdown");
+console.log(featuresDropdown);
+
+const company = document.querySelector(".company");
+console.log(company);
+
+const companyDropdown = document.querySelector(".company-dropdown");
+console.log(companyDropdown);
+
+const arrow = document.querySelectorAll(".arrow-image");
+console.log(arrow);
+
+function openFeaturesDropdown() {
+    featuresDropdown.classList.remove("hidden");
+    featuresDropdown.classList.add("show");
+    arrow[0].setAttribute("src", "../images/icon-arrow-down.svg");
+}
+
+function closeFeaturesDropdown() {
+    featuresDropdown.classList.remove("show");
+    featuresDropdown.classList.add("hidden");
+    arrow[0].setAttribute("src", "../images/icon-arrow-up.svg");
+}
+
+function openCompanyDropdown() {
+    companyDropdown.classList.remove("hidden");
+    companyDropdown.classList.add("show");
+    arrow[1].setAttribute("src", "../images/icon-arrow-down.svg");
+}
+
+function closeCompanyDropdown() {
+    companyDropdown.classList.remove("show");
+    companyDropdown.classList.add("hidden");
+    arrow[1].setAttribute("src", "../images/icon-arrow-up.svg");
+}
+
+menuButton.addEventListener("click", () => {
+    const visibility = navLinks.getAttribute("data-visible");
+    if (visibility === "false") {
+        menuButton.setAttribute("aria-expanded", true);
+        navLinks.setAttribute("data-visible", true);
+    } else if (visibility === "true") {
+        menuButton.setAttribute("aria-expanded", false);
+        navLinks.setAttribute("data-visible", false);
+    }
+});
+
+features.addEventListener("click", () => {
+    if (featuresDropdown.classList.contains("hidden")) {
+        openFeaturesDropdown();
+        closeCompanyDropdown();
+    } else {
+        closeFeaturesDropdown();
+    }
+});
+
+company.addEventListener("click", () => {
+    if (companyDropdown.classList.contains("hidden")) {
+        openCompanyDropdown();
+        closeFeaturesDropdown();
+    } else {
+        closeCompanyDropdown();
+    }
+});
